de9b7041c3e883ba4c752c53697585a361493229
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-06-17 12:14:19 +0000

    changes for operations

--- 01_basic/03_conversionoperations.js
+++ 01_basic/03_conversionoperations.js
@@ -1,7 +1,7 @@
 let score = "true"
 
-console.log(typeof score)
-console.log(typeof (score));
+// console.log(typeof score)
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
@@ -15,10 +15,49 @@ console.log(valueInNumber);  // Nan => not a number
 let islogin = ""
 
 let booleanIslogin = Boolean(islogin)
-console.log(booleanIslogin);
+// console.log(booleanIslogin);
 
 // 1=> true , 0=> false
 // "" => fasle
 // "darshan" => true
 
+ /********* Operations*************/
+
+ let value = 3
+  
+ let negValue = -value
+ // console.log(negValue);
+ 
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2/3)
+
+let str1 = "hello"
+let str2 = " darshan"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log( typeof "1" + 2);
+// console.log(1+ "2");
+// console.log("1" + 2+2);
+// console.log( typeof 1+2+"2");
+// console.log( typeof (3 + 4 * 5 / 3));
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true)
+// console.log("");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter ++
+console.log(gameCounter);
 
