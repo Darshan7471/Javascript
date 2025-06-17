dd19a4b8c89c32127040e63d87a83caed74e308f
Darshan Nikam <111945528+Darshan7471@users.noreply.github.com>
2025-06-17 10:12:03 +0000

    changes for conversion

--- 01_basic/03_conversionoperations.js
+++ 01_basic/03_conversionoperations.js
@@ -11,3 +11,12 @@ console.log(valueInNumber);  // Nan => not a number
 
 // "33" => 33
 // "33abc" => NaN
+
+let islogin = ""
+
+let booleanIslogin = Boolean(islogin)
+console.log(booleanIslogin);
+
+// 1=> true , 0=> false
+
+
